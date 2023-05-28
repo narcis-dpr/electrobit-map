62a50591304e435e20cd7a4adcdb78e3701e4c60
narcis dpr <nargesdeypir@gmail.com>
2023-05-28 23:00:31 +0330

    Git init

---
+++ app/src/main/java/com/example/electrobitemap/ui/theme/Color.kt
@@ -0,0 +1,16 @@
+package com.example.electrobitemap.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val Lavender7 = Color(0xff5500d7)
+val Lavender6 = Color(0xff6f13e4)
+val Rose6 = Color(0xffe21365)
+val Rose7 = Color(0xffd4005d)
